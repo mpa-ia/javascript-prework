16eec5832676c380d8f77e5b7e490a9f0b392c76
Marysia Pabich <marysia.pabich@gmail.com>
2019-09-07 18:18:27 +0200

    Add js function to start a new game

--- js/script.js
+++ js/script.js
@@ -50,15 +50,22 @@ function playGame (playerInput) {
     
 }
 
-document.getElementById('play-rock').addEventListener('click', function () {playGame(1)}, false);
-document.getElementById('play-paper').addEventListener('click', function () {playGame(2)}, false);
-document.getElementById('play-scissors').addEventListener('click', function () {playGame(3)}, false);
+function newGame () {
+    clearMessages();
+    computerWon = 0;
+    playerWon = 0;
+    roundNumber = 0;
+}
+
+document.getElementById('play-rock').addEventListener('click', playGame(1));
+document.getElementById('play-paper').addEventListener('click', function() {playGame(2)});
+document.getElementById('play-scissors').addEventListener('click', function () {playGame(3)});
 
 if (roundNumber >= 12) {
 
-    document.getElementById('play-rock').removeEventListener('click', function () {playGame(1)}, false);
-    document.getElementById('play-paper').removeEventListener('click', function () {playGame(2), false});
-    document.getElementById('play-scissors').removeEventListener('click', function () {playGame(3)}, false);
+    document.getElementById('play-rock').click = null;
+    document.getElementById('play-paper').click = null;
+    document.getElementById('play-scissors').click = null;
     printMessage(computerWon + ":" + playerWon);
 
     if (playerWon>computerWon) {
