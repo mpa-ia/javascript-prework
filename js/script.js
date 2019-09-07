6d7ff1b9400eec93b291de40b7cdd0d554953f07
Marysia Pabich <marysia.pabich@gmail.com>
2019-09-07 12:06:06 +0200

    Change background, adjust margins and font sizes

--- js/script.js
+++ js/script.js
@@ -6,8 +6,7 @@ function playGame (playerInput) {
     clearMessages();
 
     roundNumber++;
-    
-
+   
     function getMoveName (argNumber) {
         if(argNumber == '1'){
             return 'kamień';
@@ -22,35 +21,53 @@ function playGame (playerInput) {
     function displayResult (argComputerMove, argPlayerMove) {
         console.log('Argumenty funkcji: ', argComputerMove, argPlayerMove);
     
-        if (argComputerMove == 'kamień' && argPlayerMove == 'papier' || argComputerMove == 'papier' && argPlayerMove == 'nożyce' || argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-            printMessage('Ty wygrywasz rundę!');
+        if (argComputerMove == argPlayerMove) {
+            printMessage('Remis!');
+        }
+        else if (argComputerMove == 'kamień' && argPlayerMove == 'papier' || argComputerMove == 'papier' && argPlayerMove == 'nożyce' || argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+            printMessage('Wygrywasz rundę!');
             playerWon++;
-        } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień' || argComputerMove == 'kamień' && argPlayerMove == 'nożyce' || argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+        } else {
             printMessage('Przegrywasz rundę!');
             computerWon++;
-        } else if (argComputerMove == argPlayerMove) {
-            printMessage('Remis!');
-        } else {
-            printMessage('Coś poszło nie tak, spróbujmy jeszcze raz!');
         }
     }
     
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('Wylosowana liczba to: ' + randomNumber);
-    
+        
     let computerMove = getMoveName(randomNumber);
     printMessage(computerMove);
-
+    
     let playerMove = getMoveName(playerInput); 
     printMessage(playerMove);
+    
+    printMessage("Runda: " + roundNumber);
+    console.log('Runda: ' + roundNumber);
 
     displayResult(computerMove, playerMove);
     printMessage(computerWon + ":" + playerWon);
-    printMessage("Runda: " + roundNumber);
-
+    
 }
 
-document.getElementById('play-rock').addEventListener('click', function () {playGame(1)});
-document.getElementById('play-paper').addEventListener('click', function () {playGame(2)});
-document.getElementById('play-scissors').addEventListener('click', function () {playGame(3)});
+document.getElementById('play-rock').addEventListener('click', function () {playGame(1)}, false);
+document.getElementById('play-paper').addEventListener('click', function () {playGame(2)}, false);
+document.getElementById('play-scissors').addEventListener('click', function () {playGame(3)}, false);
+
+if (roundNumber >= 12) {
+
+    document.getElementById('play-rock').removeEventListener('click', function () {playGame(1)}, false);
+    document.getElementById('play-paper').removeEventListener('click', function () {playGame(2), false});
+    document.getElementById('play-scissors').removeEventListener('click', function () {playGame(3)}, false);
+    printMessage(computerWon + ":" + playerWon);
+
+    if (playerWon>computerWon) {
+        printMessage("Wygrałeś Grę");
+    } else if (playerWon == computerWon){
+        printMessage("Remis!");
+    } else {
+        printMessage("Przegrałeś grę!");
+    }
+    
+} 
 
